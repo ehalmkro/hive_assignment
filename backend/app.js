97630b8e2c9582f6a36d1f9de1145b4f080e3d4d
Esa Halmkrona <ehalmkro@student.hive.fi>
2020-11-19 17:52:51 +0200

    add function to get scale_teams from last 24h, removing eval sessions that lasted > 15min

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,8 @@
 import dotenv from 'dotenv';
 import cors from 'cors';
 import express from 'express';
-import { getToken, fetchAllFromAPI } from './utils/utilities.js'
-//import { remove } from lodash
+import {getToken, fetchAllFromAPI} from './utils/utilities.js'
+import _ from 'lodash'
 
 
 dotenv.config();
@@ -15,34 +15,33 @@ const evaluationController = express.Router()
 
 
 evaluationController.get('/', async (request, response, next) => {
-    response.status(200).json(userList);
+    response.status(200).json(evaluationList);
 })
 
 
 const now = new Date();
 const dayAgo = new Date();
-dayAgo.setDate(now.getDate()-1);           // TODO: replace w/ user defined time
-console.log('now it is', now, 'a week ago it was', dayAgo)
+dayAgo.setDate(now.getDate() - 1);           // TODO: replace w/ user defined time
+console.log('now it is', now, 'a day ago it was', dayAgo)
 
 
-const { access_token, expires_in }  = await getToken()
+const {access_token, expires_in} = await getToken()
 console.log(access_token, expires_in)
 
-const userList = await fetchAllFromAPI('/campus/13/users/', access_token)
-console.log(userList.length, 'users at Hive Helsinki')
+//const userList = await fetchAllFromAPI('/campus/13/users/', access_token)
+//console.log(userList.length, 'users at Hive Helsinki')
 
 await new Promise(r => setTimeout(r, 1000)); // Sleep to not go over rate limit...
 
-//const evaluationList = await fetchAllFromAPI('/scale_teams/', access_token, {range: {filled_at: `${weekAgo.toISOString()},${now.toISOString()}`}})
-
 const evaluationList = await fetchAllFromAPI(`/scale_teams?range[filled_at]=${dayAgo.toISOString()},${now.toISOString()}`, access_token)
-console.log(evaluationList)
+console.log('before', evaluationList.length)
 
-
-//const cursusList = await fetchAllFromAPI('/cursus/1/users', access_token)
-//console.log(cursusList.length, '42 cursus students')
-
-
+_.remove(evaluationList, (evaluation) => {
+    const filledAt = Date.parse(evaluation.filled_at)
+    const beginAt = Date.parse(evaluation.begin_at)
+    return ((filledAt - beginAt) / 1000 / 60) > 15
+})
+console.log('after', evaluationList.length)
 
 app.use('/api/evaluations', evaluationController)
 
