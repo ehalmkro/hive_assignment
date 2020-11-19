cf7762835cc3d624ecabe9076e642e953321d829
Esa Halmkrona <ehalmkro@student.hive.fi>
2020-11-19 13:15:20 +0200

    add lodash to parse data more clearly

--- backend/app.js
+++ backend/app.js
@@ -14,10 +14,8 @@ const evaluationController = express.Router()
 
 
 evaluationController.get('/', async (request, response, next) => {
-
+    response.status(200).json(userList);
 })
-
-app.use('/api/evaluations', evaluationController)
 
 
 const { access_token, expires_in }  = await getToken()
@@ -25,6 +23,8 @@ console.log(access_token, expires_in)
 
 const userList = await getAllCampusUsers(13, access_token)
 console.log(userList.length, 'users')
+
+app.use('/api/evaluations', evaluationController)
 
 
 export default app;
