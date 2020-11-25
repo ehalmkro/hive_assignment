061f67233b2bc81f85a72bc1d7ea55f7cd98bea8
Esa Halmkrona <ehalmkro@student.hive.fi>
2020-11-25 13:27:23 +0200

    add static front end

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ import evaluationController from "./src/evaluationController.js";
 const app = express();
 app.use(cors());
 app.use(express.json())
+app.use(express.static('build'))
 app.use('/api/evaluations', evaluationController)
 
 
